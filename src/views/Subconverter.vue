55f24382603f67c52e7fbd1bab5e6c36c010bbb7
QCEnjoyLL <klts1228@163.com>
2021-04-13 14:30:27 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,7 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-	  "sub.nerocats.cn (nerocats.cn提供）": "https://sub.nerocats.cn/sub?",
+          "sub.nerocats.cn (nerocats.cn提供）": "https://sub.nerocats.cn/sub?",
           "sub-beta.now.sh (自动编译最新版本后端-测试）": "https://sub-beta.now.sh/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
@@ -265,7 +265,7 @@ export default {
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-	  { value: "https://sub.nerocats.cn/sub?" },
+          { value: "https://sub.nerocats.cn/sub?" },
           { value: "https://sub-beta.now.sh/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
